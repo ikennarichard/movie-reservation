a2c61742059d1d530c5478c956d23ae5fe2ba64a
Ikenna Richard <oguejioforrichard@gmail.com>
2025-01-14 11:11:08 +0100

    chore: update system to service

--- handle_readiness.go
+++ handle_readiness.go
@@ -4,6 +4,6 @@ import "github.com/gin-gonic/gin"
 
 func handleReadiness(c *gin.Context) {
 	c.IndentedJSON(200, gin.H{
-		"message": "Welcome to the Movie Reservation System",
+		"message": "Welcome to the Movie Reservation Service",
 	})
 }
